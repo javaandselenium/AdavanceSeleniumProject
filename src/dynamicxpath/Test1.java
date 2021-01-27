384f819f80d5f33874aa95b64f505fd577788023
QSP <QSP@DESKTOP-EVK9GMC>
2021-01-27 15:53:32 +0530

    A

--- src/dynamicxpath/Test1.java
+++ src/dynamicxpath/Test1.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test1 {
 	public static void main(String[] args) {
-		String productName="Apple iPhone XR (Black, 64 GB) (Includes EarPods, Power Adapter)";
+		String productName="Apple iPhone XR (B, 64 GB) (Includes EarPods, Power Adapter)";
 	System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://www.flipkart.com/");
